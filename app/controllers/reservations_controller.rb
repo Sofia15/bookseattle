c00bec1d0387bce8b2af20af939890f9d86da4a1
Sofia Kim <sofiamhkim@gmail.com>
2017-07-13 20:26:14 -0700

    Add reservations#create.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,9 +9,25 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    reservation = Reservation.create!(munged_params)
+    respond_with reservation
   end
 
   def update
   end
 
+  private
+
+  def munged_params
+    duped = reservation_params.dup
+    check_in = duped.delete(:checkin).try(:to_date)
+    check_out = duped.delete(:checkout).try(:to_date)
+    duped[:reservation_duration] = check_in..check_out
+    duped
+  end
+
+  def reservation_params
+    params.require(:reservation)
+      .permit(:checkin, :checkout, :guest_count, :room_id)
+  end
 end
