e01df1be3e7f89f097f1341f2f2113957f1a9dac
Sofia Kim <sofiamhkim@gmail.com>
2017-07-16 22:28:47 -0700

    Add root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/",  to: "rooms#index"
+  root to: "rooms#index"
 
   resources :rooms, only: [:show, :index]
   resources :reservations, except: [:new, :edit]
