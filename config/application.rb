dd20f84c9b75a9167da92d829ec5d79db6736057
Sofia Kim <sofiamhkim@gmail.com>
2017-07-25 16:52:23 -0700

    Add UTC rails configuration.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Bookseattle
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'London'
+    config.active_record.default_timezone = :local
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
